a427a1b51fe25de93b7062163462ca1704f6ab51
mattsoetaert <soetaertmatt@gmail.com>
2026-04-28 12:38:41 -0700

    Fix booking page prerender build

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import BookingContent from "@/components/BookingContent";
@@ -14,7 +15,9 @@ export default function BookingPage() {
     <>
       <Navbar />
       <main>
-        <BookingContent />
+        <Suspense fallback={null}>
+          <BookingContent />
+        </Suspense>
       </main>
       <Footer />
     </>
